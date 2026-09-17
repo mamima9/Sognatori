9383eb83e953f56feaa4ea24ce7309323d3a2add
Manuel Lari <manuelylari@gmail.com>
2026-09-17 14:38:14 +0200

    okok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -417,21 +417,6 @@ useEffect(() => {
 
 {/* ARCADE WORLD */}
 {screen === "arcadeWorld" && (
-  <>
-    {isDev && (
-      <div className="fixed top-4 left-4 z-50 flex flex-wrap gap-2 max-w-xs">
-        {Array.from({ length: 9 }, (_, i) => i + 1).map((testStage) => (
-          <button
-            key={testStage}
-            onClick={() => setArcadeStage(testStage)}
-            className="px-3 py-2 rounded-lg bg-red-600 text-white text-xs font-black shadow-lg hover:bg-red-500"
-          >
-            STAGE {testStage}
-          </button>
-        ))}
-      </div>
-    )}
-
     <motion.div
       key="arcadeWorld"
       initial={{ opacity: 0 }}
